1e77e22e231bf41123b93633957bd0b7fbf1387d
Ankit <ankitjha1403@gmail.com>
2024-03-25 23:21:36 +0530

    deployment

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,32 +1,31 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
-import generateTokenAndSetCookie from "../utils/generateToken.js"; 
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
+export const signup = async (req, res) => {
+	try {
+		const { fullName, username, password, confirmPassword, gender } = req.body;
 
-export const signup = async (req,res) => {
-   try {
-    const { fullName, username, password, confirmPassword, gender } = req.body;
+		if (password !== confirmPassword) {
+			return res.status(400).json({ error: "Passwords don't match" });
+		}
 
-    if (password !== confirmPassword) {
-        return res.status(400).json({ error: "Passwords don't match" });
-    }
-
-    const user = await User.findOne({ username });
+		const user = await User.findOne({ username });
 
 		if (user) {
 			return res.status(400).json({ error: "Username already exists" });
 		}
 
-        
 		// HASH PASSWORD HERE
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
 
-        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		// https://avatar-placeholder.iran.liara.run/
+
+		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
 		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-
-        const newUser = new User({
+		const newUser = new User({
 			fullName,
 			username,
 			password: hashedPassword,
@@ -34,8 +33,7 @@ export const signup = async (req,res) => {
 			profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
 		});
 
-
-        if (newUser) {
+		if (newUser) {
 			// Generate JWT token here
 			generateTokenAndSetCookie(newUser._id, res);
 			await newUser.save();
@@ -49,41 +47,42 @@ export const signup = async (req,res) => {
 		} else {
 			res.status(400).json({ error: "Invalid user data" });
 		}
-   } catch (error) {
-    console.log("Error in signup controller", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
-   }
-}
-export const login = async (req,res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
-        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
-        
-        if (!user || !isPasswordCorrect) {
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const login = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+		if (!user || !isPasswordCorrect) {
 			return res.status(400).json({ error: "Invalid username or password" });
 		}
 
-        generateTokenAndSetCookie(user._id, res);
+		generateTokenAndSetCookie(user._id, res);
 
-        res.status(200).json({
+		res.status(200).json({
 			_id: user._id,
 			fullName: user.fullName,
 			username: user.username,
 			profilePic: user.profilePic,
 		});
-         
-    } catch (error) {
-        console.log("Error in login controller", error.message);
+	} catch (error) {
+		console.log("Error in login controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
-    }
-}
-export const logout = (req,res) => {
-    try {
+	}
+};
+
+export const logout = (req, res) => {
+	try {
 		res.cookie("jwt", "", { maxAge: 0 });
 		res.status(200).json({ message: "Logged out successfully" });
 	} catch (error) {
 		console.log("Error in logout controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
-}
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,13 +3,10 @@ import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/signup", signup);
 
-router.post("/signup" , signup )
-router.post("/login" , login)
-router.post("/logout" , logout)
+router.post("/login", login);
 
-
-
-
+router.post("/logout", logout);
 
 export default router;
